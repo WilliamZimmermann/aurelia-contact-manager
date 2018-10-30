a9d12093ddf76da900c88f667559b6b3f02ec24f
William Zimmermann <williamzimmermann@MacBook-Pro-de-William-2.local>
2018-10-30 19:04:30 -0300

    Installed dependencies like Bootstrap and FontAwesome

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # aurelia-contact-manager
+
+This project was made following the follow tutorial: https://aurelia.io/docs/tutorials/creating-a-contact-manager#building-the-application-shell
+
+## Dependencies
+
+npm install aurelia-cli -g
+npm install bootstrap --save
+npm install font-awesome --save
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import { PLATFORM } from 'aurelia-pal';
+import { Router, RouterConfiguration } from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'Contacts';
+    config.map([
+      { route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select' },
+      { route: '/contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), name:'contacts' }
+    ]);
+
+    this.router = router;
+  }
 }
